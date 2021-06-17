aca37e71314abea496055e37aa16f2d12b76afff
developer <developer@app.com>
2021-06-17 22:18:13 +0530

    add spotify style about page in mobile view

--- src/About.js
+++ src/About.js
@@ -1,12 +1,20 @@
-import SnapchatIcon from "./snapchat.svg";
-import InstagramIcon from "./instagram.svg";
-import TwitterIcon from "./twitter.svg";
-
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Slider from "@material-ui/core/Slider";
 
-import { useEffect, useState } from "react";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import InstagramIcon from "@material-ui/icons/Instagram";
+
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import FastForwardIcon from "@material-ui/icons/FastForward";
+import FastRewindIcon from "@material-ui/icons/FastRewind";
+
+import HomeIcon from "@material-ui/icons/Home";
+
+import { useEffect, useState, useRef } from "react";
 import { firestore } from "./firebase";
+import { useHistory } from "react-router-dom";
 
 const bio = `Meet Katie: a girl who eats chocolate every day and sometimes even has
 cake for breakfast. After creating a small food blog just for fun, Katie
@@ -15,8 +23,19 @@ watched the blog suddenly skyrocket in popularity, quickly becoming the
 one of the top 25 cooking websites on the internet – with up to 8
 million viewers each month.`;
 
+function getRGB(colors) {
+  return `rgb( ${colors[0]}, ${colors[1]}, ${colors[2]})`;
+}
+
 const About = (props) => {
   const [user, setUser] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  let history = useHistory();
+
+  const [background, setBackground] = useState(
+    "linear-gradient(to right, #948e99, #2e1437)"
+  );
 
   useEffect(() => {
     async function getUserInfo() {
@@ -30,65 +49,78 @@ const About = (props) => {
     getUserInfo();
   }, []);
 
+  function onRewind() {
+    history.push("/");
+  }
+
   return (
     <>
       {user && (
         <>
-          <Navbar />
-          <section className="about-container">
+          <section
+            className="about-container"
+            style={{
+              background: background ? background : "#fff",
+            }}
+          >
+            <HomeIcon className="back-button-about" onClick={onRewind} />
             <img
+              id="profilePic"
               className="about-image"
               alt="person"
               src={user.profilePicURL}
             />
-
             <h2 className="about-name">{user.name}</h2>
-            <p style={{ backgroundColor: "#c7c7c7" }} className="about-bio">
-              {user.bio.substring(0, 400)}
-            </p>
-            <footer className="about-social">
-              <a
-                href={
-                  user.instaId
-                    ? `https://www.instagram.com/${user.instaId}`
-                    : "https://www.instagram.com/instagram"
-                }
-              >
-                <img
-                  src={InstagramIcon}
-                  alt="snapchat logo"
-                  className="svg-icon"
+            <Slider
+              defaultValue={1.2}
+              min="0"
+              max="5"
+              aria-labelledby="discrete-slider-custom"
+              step={0.1}
+              style={{ width: "85%", margin: "0 auto", color: "white" }}
+            />
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-around",
+                alignItems: "center",
+                width: "100%",
+                padding: "0 2rem",
+              }}
+            >
+              <FastRewindIcon
+                onClick={onRewind}
+                style={{ fontSize: "2.5rem", cursor: "pointer" }}
+              />
+              {isPlaying ? (
+                <PauseCircleOutlineIcon
+                  style={{ fontSize: "5rem", cursor: "pointer" }}
+                  onClick={() => setIsPlaying(!isPlaying)}
                 />
-              </a>
-              <a
-                href={
-                  user.sanpchatId
-                    ? `https://www.snapchat.com/add/snapchat/${user.snapchatId}`
-                    : "https://www.snapchat.com/add/snapchat"
-                }
-              >
-                <img
-                  src={TwitterIcon}
-                  alt="twitter logo"
-                  className="svg-icon"
+              ) : (
+                <PlayCircleOutlineIcon
+                  style={{ fontSize: "5rem" }}
+                  onClick={() => setIsPlaying(!isPlaying)}
                 />
-              </a>
-              <a
-                href={
-                  user.twitterId
-                    ? `https://www.twitter.com/${user.twitterId}`
-                    : "https://www.twitter.com/twitter"
-                }
-              >
-                <img
-                  src={SnapchatIcon}
-                  alt="snapchat logo"
-                  className="svg-icon"
-                />
-              </a>
+              )}
+              <FastForwardIcon
+                style={{ fontSize: "2.5rem", cursor: "pointer" }}
+              />
+            </div>
+            {isPlaying && (
+              <p className="about-bio">{user.bio.substring(0, 400)}</p>
+            )}
+            <footer className="about-footer">
+              <TwitterIcon
+                className="svg-icon"
+                style={{ marginRight: "0.5rem" }}
+              />
+              <InstagramIcon
+                className="svg-icon"
+                style={{ marginLeft: "0.5rem" }}
+              />
             </footer>
           </section>
-          <Footer />
         </>
       )}
     </>
